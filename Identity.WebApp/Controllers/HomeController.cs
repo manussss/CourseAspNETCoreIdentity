867441df2ab5c16c55f6c21f98aae4c5b8474601
ebranquinho <branquinhomanu@gmail.com>
2025-05-27 21:00:26 -0300

    add generic methods to the controller

---
+++ Identity.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+using System.Diagnostics;
+using System.Security.Claims;
+using Identity.WebApp.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<User> _userManager;
+
+    public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
+    {
+        _logger = logger;
+        _userManager = userManager;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(CreateUserModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+
+            if (user is null)
+            {
+                user = new User()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserName = model.UserName
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+            }
+
+            return View("Success");
+        }
+
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+
+            if (user is not null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var identity = new ClaimsIdentity("cookies");
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+
+                await HttpContext.SignInAsync("cookies", new ClaimsPrincipal(identity));
+
+                return RedirectToAction("About");
+            }
+
+            ModelState.AddModelError("", "Usuário ou senha inválidos");
+        }
+
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Login()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [Authorize]
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Success()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
