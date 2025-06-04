24d214764a4d2af8ebf6cc86c8c396c91b0e8796
ebranquinho <branquinhomanu@gmail.com>
2025-06-04 06:59:59 -0300

    add dependencies injection

--- Identity.API/Program.cs
+++ Identity.API/Program.cs
@@ -1,13 +1,39 @@
+using System.Reflection;
+using Identity.Domain.Entities;
+using Identity.Infra;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services
+    .AddIdentity<User, Role>(options =>
+    {
+        options.SignIn.RequireConfirmedEmail = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 8;
+
+        options.Lockout.MaxFailedAccessAttempts = 3;
+        options.Lockout.AllowedForNewUsers = true;
+    })
+    .AddEntityFrameworkStores<WebApiDbContext>()
+    .AddRoleValidator<RoleValidator<Role>>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddDefaultTokenProviders()
+    .AddPasswordValidator<PasswordValidator<User>>();
+
+var migrationAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+builder.Services
+    .AddDbContext<WebApiDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityAPI"), sql => sql.MigrationsAssembly(migrationAssembly)));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
